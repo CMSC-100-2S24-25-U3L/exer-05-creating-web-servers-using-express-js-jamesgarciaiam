30d7c1a0bb86f97db195496e6e20639238821353
James Anthony Garcia <166273022+jamesgarciaiam@users.noreply.github.com>
2025-03-05 23:06:10 +0800

    Docs: Added documentation

--- request.js
+++ request.js
@@ -1,12 +1,12 @@
 import needle from 'needle';
 
-needle.post('http://localhost:3000/add-book', 
+needle.post('http://localhost:3000/add-book', //POST request to add the book
 {
     bookName:'Harry Potter and the Chamber of Secrets',
     isbn:'978-0-7475-3269-9',
     author:'J.K. Rowling',
     yearPublished:1998
 }, (err, res) => {
-    console.log(res.body);
+    console.log(res.body); //adds the info to response body
 });
 
